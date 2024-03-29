db14d7b5eb1efa18160c8a60ee8dd27718dc3623
Binee <ckqlss@gmail.com>
2024-03-30 02:54:49 +0800

    대시보드 페이지에 국가 목록을 표시하는 기능 추가

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -15,6 +15,17 @@ import { createClient } from '@/lib/supabase/client'
 
 export default function Dashboard() {
   const [list, setList] = useState([])
+
+  useEffect(() => {
+    const supabase = createClient()
+    const fetchData = async () => {
+      const { data } = await supabase.from('countries').select('*')
+      console.log(data)
+
+      setList((data as []) ?? [])
+    }
+    fetchData()
+  }, [])
   return (
     <>
       <Breadcrumb className="hidden md:flex">
@@ -40,7 +51,17 @@ export default function Dashboard() {
         <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
       </div>
       <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
-        <div className="flex flex-col items-center gap-1 text-center"></div>
+        <div className="flex flex-col items-center gap-1 text-center">
+          {list.map((country: any) => (
+            <div
+              key={country.id}
+              className="flex items-center justify-center gap-4"
+            >
+              <div>{country?.id}</div>
+              <div>{country?.name}</div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   )
