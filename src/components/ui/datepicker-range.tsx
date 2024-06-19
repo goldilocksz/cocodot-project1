49d950ae242cbcf59af74d026c381f181313cece
Binee <ckqlss@gmail.com>
2024-06-20 01:09:12 +0800

    update

--- src/components/ui/datepicker-range.tsx
+++ src/components/ui/datepicker-range.tsx
@@ -51,7 +51,7 @@ export function DatepickerRange({ date, setDate }: Props) {
             id="date"
             variant={'outline'}
             className={cn(
-              'w-[300px] justify-start text-left font-normal',
+              'w-[250px] justify-start text-left font-normal',
               !date && 'text-muted-foreground',
             )}
           >
