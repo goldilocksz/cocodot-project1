585958a071df4dc924b4ddc0c396b26dec336a03
Binee <ckqlss@gmail.com>
2024-06-26 13:39:31 +0800

    update

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -125,12 +125,12 @@ export default function DashboardView() {
         labels: Object.keys(titleGroup),
         datasets: [
           {
-            name: 'ATD FACTORY',
-            data: response.data.map((item: any) => item['ATD FACTORY']),
-          },
-          {
             name: 'ETD FACTORY',
             data: response.data.map((item: any) => item['ETD FACTORY']),
+          },
+          {
+            name: 'ATD FACTORY',
+            data: response.data.map((item: any) => item['ATD FACTORY']),
           },
         ],
       }
